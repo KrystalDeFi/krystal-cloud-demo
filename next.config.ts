c0e1d8caa1de1720c4cd03dee8a0b9ded551a3a7
tungpun <htung.nht@gmail.com>
2025-07-25 11:19:26 +0700

    chore: test with ts file build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
+  eslint: {ignoreDuringBuilds: true,},
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
   }
 };
 
