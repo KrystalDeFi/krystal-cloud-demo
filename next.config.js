807783f4514f75e22bfe83594034aeed3e4abd8e
tungpun <htung.nht@gmail.com>
2025-07-25 11:10:54 +0700

    chore: added next.config.js file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// Default Pages configuration for Next
+const nextConfig = {
+  images: {unoptimized: true},
+  basePath: "/krystal-cloud-demo",
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+module.exports = nextConfig
