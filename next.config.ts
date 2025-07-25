dd24d22a139cdb34ce9dbbd7541a1cb2bf8c6cdb
tungpun <htung.nht@gmail.com>
2025-07-25 10:47:42 +0700

    chore: ignore ?

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  typescript: {
+    // Ensure TypeScript errors are treated as build failures
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
